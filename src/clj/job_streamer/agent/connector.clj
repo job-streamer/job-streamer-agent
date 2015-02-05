5a6c84e530d17e2a779bc2de62f0cd3b422f8459
kawasima <kawasima1016@gmail.com>
2015-02-05 15:58:59 +0900

    Find networkinterface.

--- src/clj/job_streamer/agent/connector.clj
+++ src/clj/job_streamer/agent/connector.clj
@@ -3,7 +3,7 @@
   (:use [clojure.core.async :only [chan put! go-loop <! timeout]]
         [environ.core :only [env]])
   (:import [java.io ByteArrayOutputStream DataOutputStream File]
-           [java.net InetSocketAddress InetAddress]
+           [java.net InetSocketAddress InetAddress NetworkInterface]
            [java.nio ByteBuffer]
            [java.nio.file Files]
            [java.nio.channels DatagramChannel]))
@@ -11,19 +11,32 @@
 (def notifier-channel (chan))
 (def port-cache (atom nil))
 
+(defn write-address [address port stream]
+  (doto stream
+    (.write (.getAddress address) 0 4)
+    (.writeInt port)))
+
 (defn notify [port]
   (let [baos (ByteArrayOutputStream.)
         dos  (DataOutputStream. baos)
         ch   (DatagramChannel/open)]
-    (try 
-      (doto dos
-        (.write (.getAddress (InetAddress/getLocalHost)) 0 4)
-        (.writeInt port))
+    (try
+      (loop [i 1]
+        (when-let [interface (NetworkInterface/getByIndex i)]
+          (->> (.getInterfaceAddresses interface) 
+               (map #(.getAddress %))
+               (filter #(and (instance? java.net.Inet4Address %)
+                             (not (.isLoopbackAddress %))))
+               (map #(doto dos
+                       (.write (.getAddress %) 0 4)
+                       (.writeInt port)))
+               doall)
+          (recur (inc i))))
       (.. ch socket (setBroadcast true))
       (.send ch
         (ByteBuffer/wrap (.toByteArray baos))
         (InetSocketAddress. "255.255.255.255" (or (env :control-bus-port) 45100)))
-      (log/info "Notify multicast.")
+      (log/info "Notify broadcast.")
       (finally (.close ch)))))
 
 (defn start [port]
@@ -31,7 +44,7 @@
   (go-loop []
     (let [comm (<! notifier-channel)]
       (if (= comm :stop)
-        (log/info "stop multicast.")
+        (log/info "stop broadcast.")
         (do
           (notify port)
           (<! (timeout 10000))
