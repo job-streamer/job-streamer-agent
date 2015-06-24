a21ed2b769f2a008595229a0f0672086ce260e81
kawasima <kawasima1016@gmail.com>
2015-06-24 20:41:25 +0900

    Added a bat file for starting a notificator on windows.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.unit8.job-streamer/job-streamer-agent (slurp "VERSION")
+(defproject net.unit8.job-streamer/job-streamer-agent (clojure.string/trim-newline (slurp "VERSION")) 
   :dependencies [[javax/javaee-api "7.0"]
                  [org.jberet/jberet-core "1.1.0.Final"]
                  [org.jberet/jberet-se "1.1.0.Final"]
