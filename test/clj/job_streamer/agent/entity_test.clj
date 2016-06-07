ce9b5a1241fed845d3cbf753942e5beef3a719bb
kawasima <kawasima1016@gmail.com>
2016-06-07 14:44:33 +0900

    Fixed the problem that the nested step is parsed correctly

--- test/clj/job_streamer/agent/entity_test.clj
+++ test/clj/job_streamer/agent/entity_test.clj
@@ -1,16 +1,29 @@
-(ns entity-test
+(ns job-streamer.agent.entity-test
   (:require [clojure.edn :as edn]
-            [job-streamer.agent.entity :only [make-job to-xml]])
+            [job-streamer.agent.entity :refer [make-job to-xml]])
   (:use [clojure.test]))
 
+(def job1
+  {:job/name "j",
+   :job/components
+   [{:step/name "a",
+     :step/properties nil,
+     :step/transitions
+     [{:next/on "c",
+       :next/to
+       [{:step/name "b",
+         :step/properties nil,
+         :step/batchlet {:batchlet/ref "example.Batchlet1"}}]}],
+     :step/batchlet {:batchlet/ref "example.Batchlet2"}}], :job/properties nil})
+
 (deftest make-job-test
-  (let [job (make-job (edn/read-string "{:job/name j, :job/components [{:step/name a, :step/properties nil, :step/transitions [{:next/on c, :next/to [{:step/name b, :step/properties nil, :step/batchlet {:batchlet/ref example.HelloBatch}}]}], :step/batchlet {:batchlet/ref example.HelloBatch}}], :job/properties nil}"))]
+  (let [job (make-job job1)]
     ;success
-    (testing "一つ目のcomponentは一番初めに実行されるstepである"
-      (is (= (read-string "a") (:id (first (:components job))))))
+    (testing "First component is a first step"
+      (is (= "a" (:id (first (:components job))))))
     ;fail
-    (testing "二つ目のcomponentは二番目に実行されるstepである"
-      (is (= (read-string "b") (:id (second (:components job))))))
+    (testing "Second component is a second step"
+      (is (= "b" (:id (second (:components job))))))
     ;fail
-    (testing "一つ目のstepの中のnext/toは二番目に実行されるstepのidである"
-      (is (= (read-string "b") (:next/to (first (:transitions (first (:components job))))))))))
+    (testing "The 'next/to' of the first step is the identity of the second component."
+      (is (= "b" (:next/to (first (:transitions (first (:components job))))))))))
