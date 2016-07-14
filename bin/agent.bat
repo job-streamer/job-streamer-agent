c25c2cd54a93f14518104b05342d5e0f7ba44599
kawasima <kawasima1016@gmail.com>
2016-07-14 17:27:27 +0900

    Implements main.clj

--- bin/agent.bat
+++ bin/agent.bat
@@ -4,7 +4,6 @@ pushd %0\..\..
 
 set /p VERSION=<VERSION
 
-java -cp dist\job-streamer-agent-%VERSION%.jar;"lib\*" clojure.main -m job-streamer.agent.core
+java -cp dist\job-streamer-agent-%VERSION%.jar;"lib\*" clojure.main -m job-streamer.agent.main
 
 pause
-
--- project.clj
+++ project.clj
@@ -1,22 +1,22 @@
-(defproject net.unit8.job-streamer/job-streamer-agent (clojure.string/trim-newline (slurp "VERSION")) 
+(defproject net.unit8.job-streamer/job-streamer-agent (clojure.string/trim-newline (slurp "VERSION"))
   :dependencies [[javax/javaee-api "7.0"]
                  [org.jberet/jberet-core "1.2.0.Final"]
                  [org.jberet/jberet-se "1.2.0.Final"]
                  [org.jboss.marshalling/jboss-marshalling "1.4.10.Final"]
                  [org.jboss.logging/jboss-logging "3.3.0.Final"]
-                 
+
                  [org.jboss.weld/weld-core "2.2.16.Final"]
                  [org.jboss.weld.se/weld-se "2.2.16.Final"]
                  [net.unit8.weld/weld-prescan "0.1.0"]
-                 
+
                  [org.wildfly.security/wildfly-security-manager "1.1.2.Final"]
                  [com.google.guava/guava "19.0"]
                  [com.h2database/h2 "1.4.192"]
 
                  [org.clojure/clojure "1.8.0"]
                  [com.stuartsierra/component "0.3.1"]
-                 [duct "0.6.1"]
-                 [meta-merge "0.1.1"]
+                 [duct "0.8.0"]
+                 [meta-merge "1.0.0"]
 
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/data.xml "0.0.8"]
@@ -27,7 +27,7 @@
                  [io.undertow/undertow-websockets-jsr "1.3.22.Final"]
                  [liberator "0.14.1"]
                  [http-kit "2.1.19"]
-                 [compojure "1.5.0"]
+                 [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
                  [ring/ring-devel "1.5.0"]
                  [ch.qos.logback/logback-classic "1.1.7"]]
@@ -36,25 +36,26 @@
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
   :prep-tasks [["javac"] ["compile"]]
-   
+
   :main ^:skip-aot job-streamer.agent.main
 
   :pom-plugins [[org.apache.maven.plugins/maven-assembly-plugin "2.5.5"
                  {:configuration [:descriptors [:descriptor "src/assembly/dist.xml"]]}]]
-  
+
   :profiles
   {:dev    [:project/dev  :profiles/dev]
    :test   [:project/test :profiles/test]
    :docker [:project/docker :profiles/docker]
+   :uberjar {:aot :all}
    :profiles/dev    {}
    :profiles/test   {}
    :profiles/docker {}
-   :project/dev   {:dependencies [[duct/generate "0.6.1"]
-                                  [reloaded.repl "0.2.1"]
+   :project/dev   {:dependencies [[duct/generate "0.8.0"]
+                                  [reloaded.repl "0.2.2"]
                                   [org.clojure/tools.namespace "0.2.11"]
                                   [org.clojure/tools.nrepl "0.2.12"]
                                   [eftest "0.1.1"]
-                                  [kerodon "0.7.0"]]
+                                  [kerodon "0.8.0"]]
                    :source-paths ["dev"]
                    :jvm-opts ["-Dwscl.cache.directory=${user.home}/.wscl-cache"]
                    :repl-options {:init-ns user}
